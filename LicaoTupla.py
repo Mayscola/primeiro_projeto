2b06491be07841dd2c75f5a010f7c01602c22df7
llFelps <ll.o.pv.yuzx@gmail.com>
2024-11-19 20:03:22 -0300

    Licao 19/11

--- LicaoTupla.py
+++ LicaoTupla.py
@@ -3,6 +3,10 @@ tupla = (1,2,3,4)
 meu_set = {1,2,3,4}
 dicionario = {"nome": "Caique","idade": 22}
 
+Minha_tupla = (1,2,3,4,5)
+Minha_Lista = list( Minha_tupla)
+
+def
 
 
 def add_fruta(a,b,c):
@@ -17,6 +21,9 @@ add_fruta("Limão","Morango","Uva")
 for frutas in lista:
     print(f"Os elementos contidos na lista são: {frutas}")
 
+def del_fruta():
+    lista.pop()
+    print(lista)
 var_txt = "Paralelepipedo"
 
 for letras in var_txt:
@@ -38,3 +45,6 @@ for chave,valor in dicionario.items():
     print(dicionario["nome"])
     print(f"Chave: {chave}, Valor: {valor}")
     
+del_fruta()
+for x in lista:
+    print(f"Esta fruta está na lista {x}")    
